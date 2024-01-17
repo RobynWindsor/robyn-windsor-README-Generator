e9c67e9d8ed01fa5e58f6eb5a1c6480a9d9e9bbc
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-01-17 14:20:51 +0000

    created output folder for where generated README will be created

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// 1. Come up with README default file structure
+// 2. Decide which questions you want to ask
+// 3. Generate README.md file based on the user input
+// 4. Write a README.md file
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
